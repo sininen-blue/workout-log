8099543a8ee062985ac2c517bb7b3ecc17aaccac
sininenblue <alaanneil@gmail.com>
2026-01-31 16:18:21 +0800

    model: tag map stuff

--- src/workout_log/models.py
+++ src/workout_log/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField()
+
+
 class Exercise(models.Model):
+    tags = models.ManyToManyField(
+        Tag, through="ExerciseTagMap", related_name="exercises")
     name = models.CharField()
 
     notes = models.TextField(default="")
 
 
-class Tag(models.Model):
-    name = models.CharField()
-
-
 class ExerciseTagMap(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["tag", "exercise"],
+                name="unique_exercise_tag"
+            )
+        ]
 
 
 class Set(models.Model):
