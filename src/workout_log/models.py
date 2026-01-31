60efb111efe660f1c5cc3783a9746ea58733a3b1
sininenblue <alaanneil@gmail.com>
2026-01-31 13:33:52 +0800

    models: initial

--- src/workout_log/models.py
+++ src/workout_log/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField()
+
+    notes = models.TextField(default="")
+
+
+class Tag(models.Model):
+    name = models.CharField()
+
+
+class ExerciseTagMap(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class Set(models.Model):
+    weight = models.IntegerField()
+    reps = models.IntegerField()
+
+    date = models.DateTimeField(auto_now=True)
