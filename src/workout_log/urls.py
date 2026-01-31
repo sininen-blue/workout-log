62e8843ebd6208cc1f237314e78972df529eed8c
sininenblue <alaanneil@gmail.com>
2026-01-31 17:57:04 +0800

    misc changes

--- src/workout_log/urls.py
+++ src/workout_log/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "set"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/exercise/<int:exercise_id>", views.create, name="create"),
+    path("store", views.store, name="store"),
+    path("<int:set_id>/show", views.show, name="show"),
+    path("<int:set_id>/edit", views.edit, name="edit"),
+    path("<int:set_id>/update", views.update, name="update"),
+    path("<int:set_id>/destroy", views.destroy, name="destroy"),
 ]
