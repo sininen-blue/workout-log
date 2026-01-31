9f793d5013fc62d4054183c84dea877c3a467b52
sininenblue <alaanneil@gmail.com>
2026-01-31 16:18:40 +0800

    views: tag map views

--- src/workout_log/urls_exercise.py
+++ src/workout_log/urls_exercise.py
@@ -11,4 +11,12 @@ urlpatterns = [
     path("<int:exercise_id>/edit", views_exercise.edit, name="edit"),
     path("<int:exercise_id>/update", views_exercise.update, name="update"),
     path("<int:exercise_id>/destroy", views_exercise.destroy, name="destroy"),
+
+    # tags
+    path("<int:exercise_id>/tag/create",
+         views_exercise.create_tag, name="create_tag"),
+    path("<int:exercise_id>/tag/<int:tag_id>/store",
+         views_exercise.store_tag, name="store_tag"),
+    path("<int:exercise_id>/tag/<int:tag_id>/destroy",
+         views_exercise.destroy_tag, name="destroy_tag"),
 ]
--- src/workout_log/views_exercise.py
+++ src/workout_log/views_exercise.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
+from django.db import IntegrityError
 from django.db.models import QuerySet
 from typing import Optional
 
-from .models import Exercise
+from .models import Exercise, Tag, ExerciseTagMap
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    exercises: QuerySet[Exercise] = Exercise.objects.all()
+    exercises: QuerySet[Exercise] = Exercise.objects.prefetch_related(
+        "tags").all()
 
     context: dict[str, object] = {
         "exercises": exercises,
@@ -70,5 +72,42 @@ def update(request: HttpRequest, exercise_id: int) -> HttpResponse:
 def destroy(request: HttpRequest, exercise_id: int) -> HttpResponse:
     exercise: Exercise = get_object_or_404(Exercise, pk=exercise_id)
     exercise.delete()
+
+    return redirect("exercise:index")
+
+
+def create_tag(request: HttpRequest, exercise_id: int) -> HttpResponse:
+    exercise: Exercise = get_object_or_404(Exercise, pk=exercise_id)
+    tags: Tag = Tag.objects.all()
+
+    context: dict[str, object] = {
+        "exercise": exercise,
+        "tags": tags
+    }
+
+    return render(request, "exercise/create_tag.html", context)
+
+
+def store_tag(request: HttpRequest, exercise_id: int, tag_id: int) -> HttpResponse:
+    exercise: Exercise = get_object_or_404(Exercise, pk=exercise_id)
+    tag: Tag = get_object_or_404(Tag, pk=tag_id)
+
+    try:
+        ExerciseTagMap.objects.create(
+            exercise=exercise,
+            tag=tag,
+        )
+    except IntegrityError:
+        messages.error(request, exercise.name +
+                       " already has the tag: " + tag.name)
+        return redirect("exercise:index")
+
+    return redirect("exercise:index")
+
+
+def destroy_tag(request: HttpRequest, exercise_id: int, tag_id: int) -> HttpResponse:
+    exerciseTagMap: ExerciseTagMap = get_object_or_404(
+        ExerciseTagMap, exercise=exercise_id, tag=tag_id)
+    exerciseTagMap.delete()
 
     return redirect("exercise:index")
