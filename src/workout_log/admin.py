c12ef22cc297816447f492ddf83e88c50e7f1cfe
sininenblue <alaanneil@gmail.com>
2026-01-31 16:18:55 +0800

    admin: updated models

--- src/workout_log/admin.py
+++ src/workout_log/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Exercise, Tag, Set
+from .models import Exercise, Tag, Set, ExerciseTagMap
 
 admin.site.register(Exercise)
+admin.site.register(ExerciseTagMap)
 admin.site.register(Tag)
 admin.site.register(Set)
