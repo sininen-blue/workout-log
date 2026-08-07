f594d708a490e9640b157a2eab18c0da74a1c6b6
Neil Alaan <alaanneil@gmail.com>
2026-08-07 20:17:58 +0800

    added counter for new sets

--- src/workout_log/views_tag.py
+++ src/workout_log/views_tag.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.db.models import QuerySet
 from typing import Optional
 
-from .models import Tag
+from .models import Tag, Set
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -37,9 +38,22 @@ def store(request: HttpRequest) -> HttpResponse:
 
 def show(request: HttpRequest, tag_id: int) -> HttpResponse:
     tag: Tag = get_object_or_404(Tag, pk=tag_id)
+    exercises = tag.exercises.all()
+    sets_today = Set.objects.filter(
+        date__date=datetime.today()
+    ).all()
+
+    grouped_exercises = []
+    for exercise in exercises:
+        grouped_exercises.append({
+            "exercise": exercise,
+            "tags": exercise.tags.all(),
+            "done_count": range(_get_set_count(exercise, sets_today)),
+        })
 
     context: dict[str, object] = {
-        "tag": tag
+        "tag": tag,
+        "grouped_exercise": grouped_exercises
     }
 
     return render(request, "tag/show.html", context)
@@ -79,3 +93,13 @@ def destroy(request: HttpRequest, tag_id: int) -> HttpResponse:
         messages.error(request, "Tag delete error: " + str(e))
 
     return redirect("tag:index")
+
+
+def _get_set_count(exercise, sets):
+    count = 0
+
+    for exercise_set in sets:
+        if exercise_set.exercise == exercise:
+            count += 1
+
+    return count
